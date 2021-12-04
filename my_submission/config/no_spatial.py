d8c8269b256c1914cd21e17e7b99b690d82f3691
Yuxuan <yuxuan.xie@hotmail.com>
2021-12-04 21:02:33 +0800

    Yx: small batch

--- my_submission/config/no_spatial.py
+++ my_submission/config/no_spatial.py
@@ -1,7 +1,7 @@
 from easydict import EasyDict
 
 gobigger_dqn_config = dict(
-    exp_name='results/gobigger_no_spatial_baseline_dqn',
+    exp_name='results/vsbot',
     env=dict(
         collector_env_num=32,
         evaluator_env_num=8,
@@ -31,16 +31,16 @@ gobigger_dqn_config = dict(
         nstep=3,
         discount_factor=0.99,
         learn=dict(
-            update_per_collect=8,
-            batch_size=512,
-            learning_rate=0.0005,
+            update_per_collect=2,
+            batch_size=64,
+            learning_rate=0.0001,
             ignore_done=True,
             learner=dict(
                 log_show_after_iter =10, 
                 save_ckpt_after_iter=100,
             )
         ),
-        collect=dict(n_sample=1024, unroll_len=1),
+        collect=dict(n_sample=64, unroll_len=1),
         eval=dict(evaluator=dict(eval_freq=200, )),
         other=dict(
             eps=dict(
@@ -49,7 +49,7 @@ gobigger_dqn_config = dict(
                 end=0.1,
                 decay=100000,
             ),
-            replay_buffer=dict(replay_buffer_size=100000, ),
+            replay_buffer=dict(replay_buffer_size=50000, ),
         ),
     ),
 )
