ce865257b08f4ae47213b854ddbd631f666d29e6
Yuxuan <yuxuan.xie@hotmail.com>
2021-12-09 13:57:24 +0800

    Yx: change submission.py

--- my_submission/my_submission.py
+++ my_submission/my_submission.py
@@ -6,14 +6,61 @@ import copy
 import torch
 import time
 
-from ding.config import compile_config
-from ding.policy import DQNPolicy
 from .envs import GoBiggerEnv
-from .model import GoBiggerStructedNetwork
 from .config.no_spatial import env_config
-from gobigger.agents import BotAgent
+from .model.gb import TorchRNNModel
 
-from .entry.visuallizer import PPOBot
+import pickle
+import numpy as np
+from ray.rllib.utils.torch_utils import convert_to_torch_tensor
+
+model_config = {
+    "custom_model": "go_bigger", 
+    "lstm_cell_size": 128 ,
+    "max_seq_len" : 10,
+    "custom_model_config": {
+        "obs_shape" : 50,
+        "entity_shape" : 31,
+        "obs_embedding_size" : 128,
+        "entity_embedding_size" : 128,
+        "all_embedding_size" : 128,
+    }
+}
+
+class PPOBot():
+    def __init__(self, checkpoint_path, player_names) -> None:
+        self.checkpoint = pickle.load(open(checkpoint_path, 'rb'))
+        self.worker_info = pickle.loads(self.checkpoint['worker'])
+        self.model = TorchRNNModel(self.worker_info['policy_specs']['policy-0'].observation_space, 
+                                    self.worker_info['policy_specs']['policy-0'].action_space, 16, model_config, "PPOBot")
+        self.model.load_state_dict(convert_to_torch_tensor(self.worker_info['state']['policy-0']['weights']))
+        self.env = GoBiggerEnv(env_config)
+        self.player_names = player_names
+        self.state = self.initial_state()
+
+
+    def initial_state(self):
+        hc = [self.model.get_initial_state() for _ in range(len(self.player_names))]
+        state = [torch.cat([each[0].unsqueeze(dim=0) for each in hc], dim=0),  torch.cat([each[1].unsqueeze(dim=0) for each in hc], dim=0)]
+        return state
+
+    # input -> {id -> action}
+    def get_actions(self, obs):
+        bs_transform = self.env._obs_transform(obs)[0]
+
+        inputs = []
+        for i in range(len(self.player_names)):
+            inputs.append(np.concatenate((bs_transform[0]["scalar_obs"].reshape(1, -1), bs_transform[0]["unit_obs"].reshape(1, -1)), axis=-1))
+        inputs = torch.from_numpy(np.stack(inputs, axis=0))
+        
+        logits, self.state = self.model.forward_rnn(inputs, self.state, [1, 1, 1])
+        logits = torch.squeeze(logits, dim=1)
+        actions = torch.argmax(logits, dim=1, keepdim=False).tolist()
+        a = {f'{i}' : self.env._to_raw_action(action) for i, action in zip(self.player_names, actions)}
+        return a
+
+
+
 
 class BaseSubmission:
 
