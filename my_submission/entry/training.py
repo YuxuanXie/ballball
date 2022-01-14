762f41c1e869ff37a831120d4f5aaa605178dffd
Yuxuan <yuxuan.xie@hotmail.com>
2022-01-14 12:06:59 +0800

    Yx: config

--- my_submission/entry/training.py
+++ my_submission/entry/training.py
@@ -31,7 +31,7 @@ tf.app.flags.DEFINE_integer('num_gpus', 1, 'Number of available GPUs')
 tf.app.flags.DEFINE_boolean('use_gpus_for_workers', False, 'Set to true to run workers on GPUs rather than CPUs')
 tf.app.flags.DEFINE_boolean('use_gpu_for_driver', True, 'Set to true to run driver on GPU rather than CPU.')
 # num_workers_per_device increases and the sample time also increases. 2 is better here 
-tf.app.flags.DEFINE_float('num_workers_per_device', 2, 'Number of workers to place on a single device (CPU or GPU)')
+tf.app.flags.DEFINE_float('num_workers_per_device', 3, 'Number of workers to place on a single device (CPU or GPU)')
 tf.app.flags.DEFINE_float('num_cpus_for_driver', 1, 'Number of workers to place on a single device (CPU or GPU)')
 
 tf.app.flags.DEFINE_float('entropy_coeff', 0.00, 'The entropy')
@@ -41,8 +41,8 @@ tf.app.flags.DEFINE_string( 'restore', '', 'load model path')
 
 
 gc_default_params = {
-    'lr_init': 1e-4,
-    'lr_final': 1e-4,
+    'lr_init': 5e-5,
+    'lr_final': 5e-5,
 }
 ppo_params = {
     'entropy_coeff': 1e-5,
@@ -52,8 +52,8 @@ ppo_params = {
     "lambda" : FLAGS.lam,
     "gamma" : FLAGS.gamma,
     "clip_param" : 0.2,
-    "sgd_minibatch_size" : 1024,
-    "train_batch_size" : 1024,
+    "sgd_minibatch_size" : 3072,
+    "train_batch_size" : 3072,
     "num_sgd_iter" : 4,
     "rollout_fragment_length" : 64,
     "grad_clip" : 30,
@@ -78,7 +78,6 @@ impala_params = {
 }
 
 apex_params = {
-    "n_step": 3,
     "buffer_size": 5000000,
     # TODO(jungong) : add proper replay_buffer_config after
     #     DistributedReplayBuffer type is supported.
@@ -159,8 +158,7 @@ def setup(env, hparams, algorithm, train_batch_size, num_cpus, num_gpus,
                 "train_batch_size": train_batch_size,
                 # "horizon": 3 * one_layer_length, # it dosnot make done in step function true
                 "lr_schedule":
-                [[0, hparams['lr_init']],
-                    [1000000, hparams['lr_final']]],
+                    [[0, hparams['lr_init']],[1000000, hparams['lr_final']]],
                 "num_workers": num_workers,
                 "num_gpus": gpus_for_driver,  # The number of GPUs for the driver
                 "num_cpus_for_driver": cpus_for_driver,
@@ -235,10 +233,12 @@ def main(unused_argv):
                 },
                 'checkpoint_freq': FLAGS.checkpoint_frequency,
                 "config": config,
-                # "restore": "/Users/yuxuan/git/gobigger/my_submission/entry/results/checkpoint_000800/checkpoint-800",
-                # "restore": "~/ray_results/gb_PPO/PPO_gb_env_18f27_00000_0_2022-01-01_21-31-17/checkpoint_010500/checkpoint-10500", 
+                # "restore": "~/ray_results/gb_PPO/PPO_gb_env_abf04_00000_0_2022-01-08_10-42-50/checkpoint_022700/checkpoint-22700", 
+                # "restore": "~/ray_results/gb_PPO/PPO_gb_env_8c567_00000_0_2022-01-11_08-36-42/checkpoint_040500/checkpoint-40500",
+                "restore": "~/ray_results/gb_PPO/PPO_gb_env_9b737_00000_0_2022-01-12_15-52-36/checkpoint_051200/checkpoint-51200",
             }
         },
+        # resume=True,
     )
 
 
